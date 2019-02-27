52df1bff1b922cb3326b84b85aaa581ca9c731da
Toma Khliabets <tokotomka@gmail.com>
2019-02-27 09:39:50 +0300

    Complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    let primeFactors = getPrimeFactors(n = base, base);
+    return getMin(countOccurrences(number, primeFactors));
+};
+
+
+let getPrimeFactors = (n, base) => {
+    let arr = [], uniqueArr = [], i = 2;
+    while (n > 1 && n <= base) {
+        if (n % i === 0) {
+            arr.push(i);
+            n /= i;
+        } else if (i === 2) {
+            i++;
+        } else {
+            i += 2;
+        }
+    }
+    while (arr.length > 0) {
+        uniqueArr.push(arr.filter(e => e === arr[0]));
+        arr = arr.filter(e => e !== arr[0]);
+    }
+    return uniqueArr;
+};
+
+let countOccurrences = (num, arr) => {
+    return arr.map(e => {
+        let result = 0, pow = 1;
+        while (num / Math.pow(e[0], pow) >= 1) {
+            result += Math.floor(num / Math.pow(e[0], pow++));
+        }
+        return Math.floor(result / e.length);
+    });
+};
+
+let getMin = arr => arr.sort((a, b) => a - b)[0];
